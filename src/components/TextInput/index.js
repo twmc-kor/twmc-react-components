edbd570862f0e028242c830dae2d245e5c541c02
MOON <muneon96@naver.com>
2020-03-30 23:31:24 +0900

    TextInput_test

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function TextInput() {
-  return <div>문원입니다</div>;
+  return(
+    <div>
+      <InputStyle
+        placeholder="Input your Text"
+      />
+    </div>
+  ); 
 }
 
 export default TextInput;
+
+const InputStyle = styled.input`
+font-size: 1.25rem;
+outline: none;
+border: none;
+border-bottom: 1.75px solid #95afc0;
+`;
