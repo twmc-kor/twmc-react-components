2db956441184efc28a9b169c37897dae56a32640
deveassy <xjfmwld@gmail.com>
2020-03-30 23:31:49 +0900

    PR test_2

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
 
-function Button() {
-  return <div>eassy</div>;
+// const ButtonColor = styled.button`
+//   border-style: none;
+// //   background-color: ${props => props.color}
+// // `;
+
+// class ButtonStyle extends React.Component {
+//   render() {
+//     const { color, children } = this.props;
+//     return(
+//       <ButtonColor color={color}>{children}</ButtonColor>
+//     )
+//   };
+// }
+
+const Button = () => {
+  return (
+    <button>Click</button>
+    // <ButtonStyle color="#ffc078">Edit</ButtonStyle>
+    // <ButtonStyle color="#ff8787">Delete</ButtonStyle>
+    // <ButtonStyle color="#b197fc">Reset</ButtonStyle>
+  );
 }
 
 export default Button;
+
+
