582139355cae0974c72aa2fcfa5a0dcc7f6a27a7
MOON <muneon96@naver.com>
2020-04-03 22:44:03 +0900

    4/3 TextInput

--- src/boards/TextInput.js
+++ src/boards/TextInput.js
@@ -1,8 +1,57 @@
 import React from 'react';
 import { TextInput } from '../components';
+import styled from 'styled-components';
 
 function TextInputBoard() {
-  return <div><TextInput /></div>;
+  return (
+  <Content>
+        <P>TextField - Standard</P>
+        <Div>
+        <TextInput mode='standard'
+                  type='text'
+                  id="1"
+        />
+        <Label for="1">
+          Your Name:
+        </Label>
+        </Div>
+      <P>TextField - Filled</P>
+      <TextInput mode='filled'
+                type='text'
+      />
+
+      <P>TextField - Disabled</P>
+      <TextInput mode='disabled'
+                type='text'
+                disabled // 왜 안될ㄲㅏ요..
+      />
+  </Content>
+    );
 }
 
 export default TextInputBoard;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const P = styled.div`
+width: 300px;
+margin-bottom: 15px;
+padding: 10px;
+border-bottom: 2px solid #dee2e6;
+font-size: 30px;
+color: #495057;
+cursor: default;
+`;
+
+const Div =styled.div`
+position: relative;
+`;
+
+const Label =styled.label`
+position: absolute;
+top: 0;
+left 0;
+`;
--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -1,69 +1,35 @@
 import React from 'react';
-import TextStandard from './TextStandard';
-import TextName from './TextName';
-import TextError from './TextError';
 import styled from 'styled-components';
 
-
-class Div extends React.Component {
-  render() {
-    const { color, children } = this.props;
-    return(
-      <DivField color={color}>
-        {children}
-      </DivField>
-    );
-  }
-}
-
-
-
-function TextInput () {
+export default function Input(props) {
+  const {mode, children} = props;
   return(
-    <Container>
-      <Title>Input Text(1)</Title>
-     <Div>
-        <TextStandard />
-     </Div>
-     <Title>Input Text(2)</Title>
-     <Div>
-      <TextName />
-     </Div>
-     <Title>Input Text(3)</Title>
-     <Div>
-      <TextError />
-     </Div>
-    </Container>
-  ); 
+      <Container mode={mode}>{children}</Container>
+  )
 }
 
-export default TextInput ;
-
-
-const Container = styled.div`
+const Container = styled.input`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-`;
-
-const DivField = styled.div`
-  display: flex;
-  justify-content: center;
-  border-radius: 7px;
-  width: 93%;
-  height: 20%;
-  margin: auto;
-  margin: 20px 20px;
-  background-color: white;
-`;
-
-const Title =styled.h4`
-  display: flex;
-  justify-content: center;
-  margin: auto;
-  margin: 10px;
-  font-weight: 300;
-  font-size: 20px;
-`;
+  width: 80%;
+  padding: 12px 2px 12px 2px;
+  background-color: ${(props) => (props.mode === 'filled'
+  ? '#ecf0f1' : '#fff')};
+  border: ${(props) => (props.mode === 'disabled'
+  ? '1px solid #c0392b' : '0')};
+  border-bottom: ${(props) => {
+    if(props.mode === 'standard')
+    return '1.75px solid #2f3542'
+    if(props.mode === 'filled')
+    return '1.75px solid #2f3542'
+  }};
+  border-radius: ${(props) => {
+    if(props.mode === 'standard')
+      return '0'
+    if(props.mode === 'filled')
+      return '7.5px 7.5px 0 0'
+    if(props.mode === 'disabled')
+      return '6px'
+  }};
+  outline: 0;
+  font-size: 25px;
+  `;
