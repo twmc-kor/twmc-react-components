5295718cc9951497341fd9767c65672657112bd8
deveassy <xjfmwld@gmail.com>
2020-05-27 21:01:02 +0900

    overview 내용 추가

--- src/components/Overview/index.js
+++ src/components/Overview/index.js
@@ -4,7 +4,11 @@ import styled from 'styled-components';
 function Overview() {
   return (
     <Container>
-      <Hello style={{position:"absolute", top:-10, left:-10}}>Hello, World!</Hello>
+      <Hello>
+        Hello, World!
+        <br />
+        We are Front-end Developer.
+        </Hello>
       <Dev>
         <Img src="/img/eunji.JPG" alt="LEEEUNJI profile img"/>
         <Contact>
@@ -21,6 +25,13 @@ function Overview() {
           <P href="https://velog.io/@jangmoonwon"><img src="/img/velog.jpg" alt="velog" style={{width:24, height:24}} /> VELOG</P>
         </Contact>
       </Dev>
+      <Text>
+        This Project is to modularzing our 
+        <br />
+        <strong>own components</strong> that can be used externally.
+        <br />
+        Then, Have fun ☺︎
+      </Text>
     </Container>
   );
 }
@@ -30,33 +41,37 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  width: 650px;
+  height: 500px;
 `;
 const Hello = styled.div`
   position: absolute;
-  top: -10;
-  left: -10;
-  font: 500 20px 'Chelsea Market', cursive;
+  top: 10px;
+  left: 20px;
+  font: 500 27px 'Chelsea Market', cursive;
 `;
 const Dev = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin: 30px 20px 0;
+  margin: -50px 10px 0;
+  padding: 10px 0;
 `;
 const Img = styled.img`
-  width: 250px;
-  height: 250px;
+  width: 120px;
+  height: 120px;
   border: 1px solid #000;
   border-radius: 50%;
 `;
 const Name = styled.p`
+  margin: 5px;
   font: 800 25px 'Chelsea Market', cursive;
   text-shadow: 1px 1px 1px #000;
 `;
 const P = styled.a`
   font-size: 23px;
-  margin: 10px;
+  margin: 5px;
   text-decoration: none;
   color: #000;
   font-family: 'Chelsea Market', cursive;
@@ -69,8 +84,16 @@ const Contact = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  z-index: 1;
   width: 250px;
   font-size: 25px;
+`;
+const Text = styled.div`
+  position: absolute;
+  top: 370px;
+  left: 20px;
+  margin-top: 10px;
+  font: 300 25px 'Chelsea Market', cursive;
 `;
 
 export default Overview;
