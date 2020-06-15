467620d902066ae64a5f7c90a539cf952a211f92
EUNJI LEE <58814562+deveassy@users.noreply.github.com>
2020-06-16 02:22:45 +0900

    [@eunji/Edit_checkbox] 전체 틀 수정 (#14)
    * checkbox component 전체 틀 수정
    
    * 1차 코드정리
    
    * 2차 코드 정리, check시 img 구현기능 추가
    
    * Update component could get an asset element as an option
    
    related issue #16
    
    * 선택적 checkbox 구현 수정
    
    * Add dependency for styled-components
    
    * Enhance component to be handle more props
    
    * Resolve merge conflict with upstream master
    
    Co-authored-by: devethan <devohno.ko@gmail.com>

--- src/components/CheckBox/index.js
+++ src/components/CheckBox/index.js
@@ -1,62 +1,117 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-const Checkbox = ({ isClicked, isChanged, isClickC, onClick, onChange, checked="false", ...props }) => (
-  <CheckboxContainer>
-    <HiddenCheckbox checked={checked} {...props} 
-      onClick={e => {
-        if(isClicked !== undefined) isClicked(e.target.checked);
-        if(isChanged !== undefined) isChanged(e.target.checked);
-        if(isClickC !== undefined) isClickC(e.target.checked);
-      }}/>
-    <StyledCheckbox checked={checked}>
-      <Icon viewBox="0 0 24 24">
-        <polyline points="20 6 9 17 4 12" />
-      </Icon>
-    </StyledCheckbox>
-  </CheckboxContainer>
-)
-
-const CheckboxContainer = styled.div`
-  display: inline-block;
-  vertical-align: middle;
+const CheckboxContainer = styled.label`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 5px;
 `;
 
-const Icon = styled.svg`
-  fill: none;
-  stroke: white;
-  stroke-width: 2px;
-`;
-
-const HiddenCheckbox = styled.input.attrs({ type: 'checkbox' })`
+const HiddenCheckbox = styled.input.attrs({type: 'checkbox'})`
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  padding: 0;
+  margin: -1px;
   border: 0;
   clip: rect(0 0 0 0);
   clip-path: inset(50%);
-  height: 1px;
-  margin: -1px;
   overflow: hidden;
-  padding: 0;
-  position: absolute;
   white-space: nowrap;
-  width: 1px;
+`;
+
+const StyledIcon = styled.svg`
+  fill: none;
+  stroke: black;
+  stroke-width: 3px;
+  width: 100%;
+  height: 100%;
 `;
 
 const StyledCheckbox = styled.div`
-  display: inline-block;
-  width: 16px;
-  height: 16px;
-  margin-left: 10px;
-  background: ${props => (props.checked ? '#fab1a0' : 'papayawhip')};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20px;
+  height: 20px;
+  margin: 0 5px;
+  background: ${({checked, colorSet}) =>
+    checked ? colorSet.ACTIVE_BACKGROUND : colorSet.INACTIVE_BACKGROUND};
   border-radius: 3px;
-  transition: all 150ms;
+  transition: all 130ms;
+  box-shadow: 0 0 0 2px ${({colorSet}) => colorSet.INACTIVE_BORDER};
 
   ${HiddenCheckbox}:focus + & {
-    box-shadow: 0 0 0 3px pink;
-  };
-
-  ${Icon} {
-    visibility: ${props => (props.checked ? 'visible' : 'hidden')};
-  };
+    box-shadow: 0 0 0 2px ${({colorSet}) => colorSet.ACTIVE_BORDER};
+  }
 `;
 
-export default Checkbox;
+const CheckboxItemContainer = styled.div`
+  width: 20px;
+  height: 20px;
+  visibility: ${(props) => (props.checked ? 'visible' : 'hidden')};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2px;
+`;
+
+export default function CheckBox(props) {
+  const {
+    onClick,
+    asset,
+    value = false,
+    inActiveBackgroundColor = 'papayawhip',
+    activeBackgroundColor = '#fab1a0',
+    inActiveBorderColor = '#ddd',
+    activeBorderColor = '#ff7675',
+  } = props;
+
+  const CheckboxItem = () => (
+    <CheckboxItemContainer checked={value}>
+      {asset || (
+        <StyledIcon viewBox="0 0 24 24">
+          <polyline points="20 6 9 17 4 12" />
+        </StyledIcon>
+      )}
+    </CheckboxItemContainer>
+  );
+
+  const COLOR_SET = React.useMemo(() => {
+    return {
+      INACTIVE_BACKGROUND: inActiveBackgroundColor,
+      ACTIVE_BACKGROUND: activeBackgroundColor,
+      INACTIVE_BORDER: inActiveBorderColor,
+      ACTIVE_BORDER: activeBorderColor,
+    };
+  }, [
+    inActiveBackgroundColor,
+    activeBackgroundColor,
+    inActiveBorderColor,
+    activeBorderColor,
+  ]);
+
+  return (
+    <CheckboxContainer>
+      <HiddenCheckbox {...props} onClick={(e) => onClick(e.target.checked)} />
+      <StyledCheckbox checked={value} colorSet={COLOR_SET}>
+        <CheckboxItem />
+      </StyledCheckbox>
+      {props.label}
+    </CheckboxContainer>
+  );
+}
+
+CheckBox.propTypes = {
+  value: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+  asset: PropTypes.element,
+  inActiveBackgroundColor: PropTypes.string,
+  activeBackgroundColor: PropTypes.string,
+  activeBorderColor: PropTypes.string,
+  inActiveBorderColor: PropTypes.string,
+  label: PropTypes.element.isRequired,
+};
