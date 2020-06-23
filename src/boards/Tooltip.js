feb3fff4e283a74974e4f2844355f0aa6d1d54fb
deveassy <xjfmwld@gmail.com>
2020-06-23 22:33:22 +0900

    props 세부작업 진행중

--- src/boards/Tooltip.js
+++ src/boards/Tooltip.js
@@ -4,16 +4,58 @@ import styled from 'styled-components';
 
 export default function TooltipBoard() {
   return (
-    <Container>
-      <Tooltip color>
-        이게 툴팁입니다. 알겠습니까? 만드는중이라고요 좀 기다려봐요
-      </Tooltip>
-    </Container>
+    <TooltipContainer>
+      <TooltipMode>Bright Mode</TooltipMode>
+      <BrightMode>
+        <Tooltip color mode="bright" label="Hover me!">
+          This is bright mode.
+        </Tooltip>
+      </BrightMode>
+      <TooltipMode style={{top: 290}}>Dark Mode</TooltipMode>
+      <DarkMode>
+        <Tooltip mode="dark" label="Hover me again!">
+          This is dark mode.
+        </Tooltip>
+      </DarkMode>
+    </TooltipContainer>
   );
 }
 
-const Container = styled.div`
+const TooltipContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const TooltipMode = styled.div`
+  position: absolute;
+  top: 50px;
+  left: 30px;
+  font: 600 30px 'Bangers', cursive;
+  letter-spacing: 0.1em;
+  color: #112d4e;
+`;
+
+const BrightMode = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  width: 450px;
+  height: 150px;
+  margin: 50px 0 0;
+  background-color: yellowgreen;
+`;
+const DarkMode = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 450px;
+  height: 150px;
+  margin: 90px 0 0;
+  color: #fff;
+  background-color: #555;
 `;
--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -4,11 +4,17 @@ import PropTypes from 'prop-types';
 import './style.css';
 
 export default function Tooltip(props) {
-  const {color, children} = props;
+  const {color, mode, label, children} = props;
+  // const colorProps = useMemo(() => {
+  //   if(mode === "bright") return "#666"
+  //   if(mode === "dark") return "#f5f5f5"
+  //   return
+  // })
   return (
     <Container className="tooltip">
-      Hover me!
-      <TooltipText className="tooltipText" color={color}>
+      {/* Hover me! */}
+      {label}
+      <TooltipText className="tooltipText" color={color} mode={mode}>
         {children}
       </TooltipText>
     </Container>
@@ -18,14 +24,12 @@ export default function Tooltip(props) {
 const Container = styled.div`
   position: relative;
   display: inline-block;
-  /* display: flex;
+  display: flex;
   align-items: center;
-  justify-content: center; */
-  width: 200px;
-  height: 50px;
+  justify-content: center;
+  /* width: 200px;
+  height: 50px; */
   text-align: center;
-  background-color: tomato;
-  border-bottom: 1px dotted black;
   font: 800 30px 'sans-seridf';
 `;
 const TooltipText = styled.span`
@@ -34,12 +38,18 @@ const TooltipText = styled.span`
   visibility: hidden;
   bottom: 130%;
   left: 50%;
-  width: 200px;
-  padding: 5px 0;
+  /* width: 200px; */
+  padding: 5px 10px;
   margin-left: -60px;
-  background-color: ${(props) => (props.color ? 'blue' : 'tomaot')};
+  background-color: ${(props) => {
+    if (props.mode === 'bright') return '#666';
+    if (props.mode === 'dark') return '#f5f5f5';
+  }};
   font: 500 15px 'sans-serif';
-  color: #fff;
+  color: ${(props) => {
+    if (props.mode === 'bright') return '#f5f5f5';
+    if (props.mode === 'dark') return '#333';
+  }};
   text-align: center;
   border-radius: 6px;
   opacity: 0;
@@ -51,12 +61,22 @@ const TooltipText = styled.span`
     top: 100%;
     left: 50%;
     margin-left: -5px;
-    border: 5px solid blue;
-    border-color: blue transparent transparent transparent;
+    border: ${(props) => {
+      if (props.mode === 'bright') return '5px solid #666';
+      if (props.mode === 'dark') return '5px solid #f5f5f5';
+    }};
+    border-color: ${(props) => {
+      if (props.mode === 'bright')
+        return '#666 transparent transparent transparent';
+      if (props.mode === 'dark')
+        return '#f5f5f5 transparent transparent transparent';
+    }};
   }
 `;
 
 Tooltip.propTypes = {
   children: PropTypes.node.isRequired,
   color: PropTypes.string,
+  mode: PropTypes.string,
+  label: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
 };
