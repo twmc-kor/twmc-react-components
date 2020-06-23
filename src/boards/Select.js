1afaeda8c3ef88527e282674a2f92dacbb34c887
MOON <muneon96@naver.com>
2020-06-23 22:32:08 +0900

    [Select] 전체 틀 2차 재수정(Button 기능 추가, alert 설정)

--- src/boards/Select.js
+++ src/boards/Select.js
@@ -7,11 +7,87 @@ const firstList = firstOption.map((fitstOption) => (
   <option>{fitstOption}</option>
 ));
 
+const secondOption = [
+  '01',
+  '02',
+  '03',
+  '04',
+  '05',
+  '06',
+  '07',
+  '08',
+  '09',
+  '10',
+  '11',
+  '12',
+];
+const secondList = secondOption.map((secondOption) => (
+  <option>{secondOption}</option>
+));
+
+const thirdOption = [
+  '00',
+  '05',
+  '10',
+  '15',
+  '20',
+  '25',
+  '30',
+  '35',
+  '40',
+  '45',
+  '50',
+  '55',
+];
+const thirdList = thirdOption.map((thirdOption) => (
+  <option>{thirdOption}</option>
+));
+
 function SelectBoard() {
+  const [state, setState] = React.useState({
+    firstOption: 'AM',
+    secondOption: '01',
+    thirdOption: '00',
+  });
+  const handleChange = (e) => {
+    const name = e.target.name;
+    setState({
+      ...state,
+      [name]: e.target.value,
+    });
+  };
+  const handleClick = (e) => {
+    e.preventDefault();
+    alert('알람이 설정되었습니다.');
+  };
   return (
     <Container>
-      <Select>
+      <Content>
+      <Select
+        value={state.firstOption}
+        onChange={handleChange}
+        name="firstOption">
+        {firstList}
       </Select>
+      <Select
+        value={state.secondOption}
+        onChange={handleChange}
+        name="secondOption">
+        {secondList}
+      </Select>
+      <Select
+        value={state.thirdOption}
+        onChange={handleChange}
+        name="thirdOption">
+        {thirdList}
+      </Select>
+      </Content>
+      <Content>
+      <p>
+        {state.firstOption} {state.secondOption}: {state.thirdOption}
+      </p>
+      <button onClick={handleClick}>설정하기</button>
+      </Content>
     </Container>
   );
 }
@@ -21,6 +97,13 @@ export default SelectBoard;
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
 `;
--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -12,76 +12,18 @@ const StyledSelect = styled.select`
   margin: 10px;
 `;
 
-const firstOption = ['AM', 'PM'];
-const firstList = firstOption.map((fitstOption) => (
-  <option>{fitstOption}</option>
-));
-
-const secondOption = [
-  '01',
-  '02',
-  '03',
-  '04',
-  '05',
-  '06',
-  '07',
-  '08',
-  '09',
-  '10',
-  '11',
-  '12',
-];
-const secondList = secondOption.map((secondOption) => (
-  <option>{secondOption}</option>
-));
-
-const thirdOption = [
-  '00',
-  '05',
-  '10',
-  '15',
-  '20',
-  '25',
-  '30',
-  '35',
-  '40',
-  '45',
-  '50',
-  '55',
-];
-const thirdList = thirdOption.map((thirdOption) => (
-  <option>{thirdOption}</option>
-));
-
 export default function Select(props) {
-  const {label, value, option, map, onChange} = props;
+  const {name, value, onClick, onChange, children} = props;
 
   return (
     <Container>
       <form>
         <StyledSelect
-          label={label}
+          name={name}
           value={value}
-          option={option}
-          map={map}
+          onClick={onClick}
           onChange={onChange}>
-            {firstList}
-          </StyledSelect>
-          <StyledSelect
-          label={label}
-          value={value}
-          option={option}
-          map={map}
-          onChange={onChange}>
-            {secondList}
-          </StyledSelect>
-          <StyledSelect
-          label={label}
-          value={value}
-          option={option}
-          map={map}
-          onChange={onChange}>
-            {thirdList}
+            {children}
           </StyledSelect>
       </form>
     </Container>
@@ -90,5 +32,5 @@ export default function Select(props) {
 
 const Container = styled.div`
   position: relative;
-  margin: 15px;
+  margin: 5px;
 `;
