7609603fcc081ee39e4576ed30ec464271820ff6
MOON <muneon96@naver.com>
2020-06-23 17:35:44 +0900

    [Select] 전체 틀 재수정

--- src/boards/Select.js
+++ src/boards/Select.js
@@ -1,58 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import Select from '../components/Select';
+
+const firstOption = ['AM', 'PM'];
+const firstList = firstOption.map((fitstOption) => (
+  <option>{fitstOption}</option>
+));
 
 function SelectBoard() {
-  const [state, setState] = React.useState({
-    firstOption: '',
-    secondOption: '',
-  });
-  const handleChange = (e) => {
-    const name = e.target.name;
-    setState({
-      ...state,
-      [name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert('First Option: ' + state.firstOption);
-    alert('Second Option: ' + state.secondOption);
-  };
-
   return (
     <Container>
-      <form onSubmit={handleSubmit}>
-        <Section>
-          <Select
-            value={state.firstOption}
-            onChange={handleChange}
-            name="firstOption">
-            <option value="" selected>
-              First Option
-            </option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-          </Select>
-        </Section>
-        <Section>
-          <Select
-            value={state.secondOption}
-            onChange={handleChange}
-            name="secondOption">
-            <option value="" selected>
-              Second Option
-            </option>
-            <option value="A">A</option>
-            <option value="B">B</option>
-            <option value="C">C</option>
-          </Select>
-        </Section>
-        <Section>
-          <Button type="submit" value="선택하기" />
-        </Section>
-      </form>
+      <Select>
+      </Select>
     </Container>
   );
 }
@@ -64,40 +23,4 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`;
-
-const Section = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Select = styled.select`
-background-color: transparent;
-  width: 250px;
-  height: 40px;
-  margin: 5px;
-  font-size: 17px;
-  text-align-last: center;
-  &:hover {
-    cursor: pointer;
-  }
-  &:focus {
-    outline: none;
-  }
-`;
-
-const Hr = styled.hr`
-  width: 70vh;
-`;
-
-const Button = styled.input.attrs((props) => ({type: 'submit'}))`
-  width: 250px;
-  height: 40px;
-  margin: 5px;
-  font-size: 17px;
-  &:hover {
-    cursor: pointer;
-  }
 `;
--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -1,16 +1,89 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const StyledSelect = styled.select`
+  width: 100px;
+  height: 40px;
+  font-size: 15px;
+  text-align-last: center;
+  border: 0;
+  border-bottom: 1px solid #131313;
+  background-color: transparent;
+  margin: 10px;
+`;
+
+const firstOption = ['AM', 'PM'];
+const firstList = firstOption.map((fitstOption) => (
+  <option>{fitstOption}</option>
+));
+
+const secondOption = [
+  '01',
+  '02',
+  '03',
+  '04',
+  '05',
+  '06',
+  '07',
+  '08',
+  '09',
+  '10',
+  '11',
+  '12',
+];
+const secondList = secondOption.map((secondOption) => (
+  <option>{secondOption}</option>
+));
+
+const thirdOption = [
+  '00',
+  '05',
+  '10',
+  '15',
+  '20',
+  '25',
+  '30',
+  '35',
+  '40',
+  '45',
+  '50',
+  '55',
+];
+const thirdList = thirdOption.map((thirdOption) => (
+  <option>{thirdOption}</option>
+));
+
 export default function Select(props) {
-  const [label, value, name, onChange, multiple, children] = props;
+  const {label, value, option, map, onChange} = props;
+
   return (
-    <Container
-      label={label}
-      value={value}
-      name={name}
-      onChange={onChange}
-      multiple={multiple}>
-      {children}
+    <Container>
+      <form>
+        <StyledSelect
+          label={label}
+          value={value}
+          option={option}
+          map={map}
+          onChange={onChange}>
+            {firstList}
+          </StyledSelect>
+          <StyledSelect
+          label={label}
+          value={value}
+          option={option}
+          map={map}
+          onChange={onChange}>
+            {secondList}
+          </StyledSelect>
+          <StyledSelect
+          label={label}
+          value={value}
+          option={option}
+          map={map}
+          onChange={onChange}>
+            {thirdList}
+          </StyledSelect>
+      </form>
     </Container>
   );
 }
